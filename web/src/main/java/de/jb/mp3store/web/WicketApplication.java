21bf4a332b07e9714877c9e9695956660821652c
Johannes Bayer <info@johannebayer.de>
2011-10-10 20:42:03 +0200

    Initialer import

---
+++ web/src/main/java/de/jb/mp3store/web/WicketApplication.java
@@ -0,0 +1,31 @@
+package de.jb.mp3store.web;
+
+import org.apache.wicket.protocol.http.WebApplication;
+
+/**
+ * Application object for your web application. If you want to run this application without deploying, run the Start class.
+ * 
+ * @see de.jb.mp3store.web.Start#main(String[])
+ */
+public class WicketApplication extends WebApplication
+{    	
+	/**
+	 * @see org.apache.wicket.Application#getHomePage()
+	 */
+	@Override
+	public Class<HomePage> getHomePage()
+	{
+		return HomePage.class;
+	}
+
+	/**
+	 * @see org.apache.wicket.Application#init()
+	 */
+	@Override
+	public void init()
+	{
+		super.init();
+
+		// add your configuration here
+	}
+}
